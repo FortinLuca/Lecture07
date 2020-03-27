5b8b41461d8bba4d7a80d8e99fac092d524901c5
Luca Fortin <fortluca96@gmail.com>
2020-03-27 23:50:23 +0100

    lecture09

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 (function () {
   'use strict';
+  angular.module('DIApp', [])
 
-  
+  .controller('DIController', function ($scope) {
+
+    function DIcontroller($scope) {
+      $scope.name = "LF14";
+    }
+
 
 })();
