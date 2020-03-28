4a97528bc5941675e616bcc04b7170b4501319bd
Luca Fortin <fortluca96@gmail.com>
2020-03-28 10:45:28 +0100

    Lecture09

--- app.js
+++ app.js
@@ -3,16 +3,22 @@
 
   angular.module('DIApp', [])
 
-  .controller('DIController', DIController);
+  .controller('DIController', function($scope, $filter){
 
-    function DIcontroller($scope, $filter) {
-      $scope.name = "LF14";
-      $scope.upper = function() {
-        //var upCase = $filter('uppercase');
-        $scope.name = angular.uppercase($scope.name);
-      };
+  // function DIcontroller($scope, $filter) {
+    $scope.name = "LF14";
+    $scope.upper = function() {
+      var upCase = $filter('uppercase');
+      $scope.name = upCase($scope.name);
     };
 
+    function AnnonateMe(name, job, blah){
+    return "blah";
+    };
+
+    console.log(AnnonateMe());
+
+  });
 
 
 })();
