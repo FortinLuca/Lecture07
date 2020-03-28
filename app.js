4ac08f56f4d1ff2a3712c5eee396abbb54e090aa
Luca Fortin <fortluca96@gmail.com>
2020-03-28 12:45:27 +0100

    Lecture11

--- app.js
+++ app.js
@@ -4,9 +4,13 @@
   angular.module('MsgApp', [])
   .controller('MsgController', MsgController);
 
-  MsgController.$inject = [''$scope'];
+  MsgController.$inject = ['$scope'];
   function MsgController($scope) {
     $scope.name = "LF14";
+
+    $scope.sayMessage  = function() {
+      return "LF14 likes to drink";
+    }
   }
 
 })();
