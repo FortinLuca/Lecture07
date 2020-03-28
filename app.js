ae5c0704f0f02144a7aaae52b5d2bd79fd30fb91
Luca Fortin <fortluca96@gmail.com>
2020-03-28 23:09:48 +0100

    module2

--- app.js
+++ app.js
@@ -4,18 +4,20 @@
   angular.module('MsgApp', [])
   .controller('MsgController', MsgController);
 
-  MsgController.$inject = ['$scope'];
-  function MsgController($scope) {
+  MsgController.$inject = ['$scope', '$flter'];
+  function MsgController($scope, $filter) {
     $scope.name = "LF14";
     $scope.stateOfBeing = "Pikatchu";
 
     $scope.sayMessage  = function() {
-      return "LF14 shows a pokemon!";
+      var msg = "LF14 shows a pokemon!";
+      var output = $filter('uppercase')(msg);
+      return output;
     };
 
     $scope.feedLF14 = function () {
       $scope.stateOfBeing = "charizard";
-    }
+    };
   }
 
 })();
